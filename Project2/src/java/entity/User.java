108509ebe092540679574393a54f10a38506006d
S3amusRyan <125382510+S3amusRyan@users.noreply.github.com>
2023-04-21 15:52:21 +0100

    New updates

--- Project2/src/java/entity/User.java
+++ Project2/src/java/entity/User.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
